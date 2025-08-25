4c06922b647cc5a1f3246627f1155dffdebceb6b
midul <midulislamrasel01@gmail.com>
2025-08-25 16:11:41 +0600

    Hepl er view page

--- src/app/(main)/help/page.tsx
+++ src/app/(main)/help/page.tsx
@@ -1,22 +1,22 @@
+"use client";
+
+
+
 import {
-  CustomerIcon,
   ProfileIcon,
-  RedCarpetRewardsIcon,
-  TalentIcon,
   LampIcon,
+  TalentIcon,
+  RedCarpetRewardsIcon,
+  CustomerIcon,
 } from "@/icons";
 import { Search } from "lucide-react";
-
-import React from "react";
-
 const HelpPage = () => {
   return (
-    <div className="min-h-screen  text-white flex flex-col items-center py-16 px-6">
-      {/* Title */}
+    <div className="min-h-screen text-white flex flex-col items-center py-16 px-6">
       <h1 className="text-3xl font-bold mb-6">Hi, how can we help you?</h1>
 
       {/* Search Bar */}
-      <div className="flex items-center">
+      <div className="max-w-[480px] w-full">
         <div className="relative">
           <Search className="absolute w-4.5 h-4.5 left-3 top-1/2 -translate-y-1/2 text-white/40" />
           <input
@@ -29,24 +29,19 @@ const HelpPage = () => {
 
       {/* Quick Actions */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 max-w-3xl w-full">
-        <div className="bg-white/10 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
+        <div className=" bg-white/5 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
           <div className="flex flex-col items-center justify-center p-6 text-center">
-            <span className="text-green-400 text-2xl mb-3">
-              {" "}
-              <LampIcon size={64} />
-            </span>
+            <LampIcon size={64} />
             <h2 className="font-semibold mb-2">Browse articles</h2>
             <p className="text-gray-400 text-sm">
-              Explore How-To's and learn best practices from our knowledge base
+              Explore How-To&apos;s and learn best practices from our knowledge base
             </p>
           </div>
         </div>
 
-        <div className="bg-white/10 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
+        <div className=" bg-white/5 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
           <div className="flex flex-col items-center justify-center p-6 text-center">
-            <span className="text-green-400 text-2xl mb-3">
-              <ProfileIcon size={64} />
-            </span>
+            <ProfileIcon size={64} />
             <h2 className="font-semibold mb-2">Submit a ticket</h2>
             <p className="text-gray-400 text-sm">
               Describe your issue by filling out the support ticket form
@@ -56,36 +51,104 @@ const HelpPage = () => {
       </div>
 
       {/* View All Articles */}
-      <button className="text-purple-500 mt-6 text-base hover:underline">
+      <a
+        href="/help/knowledge-base"
+        className="text-purple-500 mt-16 text-base hover:underline"
+      >
         View all articles
-      </button>
+      </a>
 
       {/* Categories */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 max-w-5xl w-full">
-        <div className="bg-white/10 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
+        <div className=" bg-white/5 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
           <div className="flex flex-col items-center justify-center p-6 text-center">
-            <span className="text-gray-400 text-2xl mb-3">
-              <CustomerIcon size={44} />
+            <span>
+              <CustomerIcon size={54} />
             </span>
-            <h2 className="font-medium">I'm a Cameo customer</h2>
+            <h2 className="font-medium mt-2">I&apos;m a Antylst customer</h2>
           </div>
         </div>
 
-        <div className="bg-white/10 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
+        <div className=" bg-white/5 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
           <div className="flex flex-col items-center justify-center p-6 text-center">
-            <span className="text-gray-400 text-2xl mb-3">
-              <TalentIcon size={44} />
+            <span>
+              {" "}
+              <TalentIcon size={54} />
             </span>
-            <h2 className="font-medium">I'm a Talent on Cameo</h2>
+            <h2 className="font-medium mt-2">I&apos;m a Talent on Antylst</h2>
           </div>
         </div>
 
-        <div className="bg-white/10 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
+        <div className="bg-white/5 border border-white/10 hover:border-purple-500 transition cursor-pointer rounded-lg">
           <div className="flex flex-col items-center justify-center p-6 text-center">
-            <span className="text-gray-400 text-2xl mb-3">
-              <RedCarpetRewardsIcon size={44} />
+            <span>
+              {" "}
+              <RedCarpetRewardsIcon size={54} />
             </span>
-            <h2 className="font-medium">Red Carpet Rewards</h2>
+            <h2 className="font-medium mt-2">Red Carpet Rewards</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-5xl mt-16">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-xl font-semibold">Most popular articles</h2>
+          <button className="text-purple-500 text-sm hover:underline">
+            View all articles
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="p-6">
+            <a
+              href="#"
+              className="flex items-start gap-2 text-purple-400 hover:underline"
+            >
+              <span>📄</span>
+              <span>Can I download my Antylst video?</span>
+            </a>
+            <p className="text-gray-400 text-sm mt-1">
+              Modified on Tue, 20 Aug, 2024 at 1:46 PM
+            </p>
+          </div>
+
+          <div className="p-6 ">
+            <a
+              href="#"
+              className="flex items-start gap-2 text-purple-400 hover:underline"
+            >
+              <span>📄</span>
+              <span>How can I check the status of my Antylst video?</span>
+            </a>
+            <p className="text-gray-400 text-sm mt-1">
+              Modified on Wed, 11 Jun at 9:59 PM
+            </p>
+          </div>
+
+          <div className="p-6 ">
+            <a
+              href="#"
+              className="flex items-start gap-2 text-purple-400 hover:underline"
+            >
+              <span>📄</span>
+              <span>When will I be charged for my Antylst video?</span>
+            </a>
+            <p className="text-gray-400 text-sm mt-1">
+              Modified on Wed, 19 Oct, 2022 at 1:06 PM
+            </p>
+          </div>
+
+          <div className="p-6 ">
+            <a
+              href="#"
+              className="flex items-start gap-2 text-purple-400 hover:underline"
+            >
+              <span>📄</span>
+              <span>How do I share my Antylst video?</span>
+            </a>
+            <p className="text-gray-400 text-sm mt-1">
+              Modified on Thu, 10 Apr at 12:53 PM
+            </p>
           </div>
         </div>
       </div>
