b00a4478cf02d236396067e2d3c7d19a72587799
Md Rifajul Islam (Rifaj) <devrifaj@gmail.com>
2025-09-10 14:41:52 +0600

    feat: update

--- src/app/dashboard/tiers/page.tsx
+++ src/app/dashboard/tiers/page.tsx
@@ -172,3 +172,11 @@ const ArtistTiersPage = () => {
 
 export default ArtistTiersPage;
  */
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
